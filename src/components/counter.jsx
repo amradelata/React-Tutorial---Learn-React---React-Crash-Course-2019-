a39b7a3330a8c10fcb301f9033fb8ba095161a2c
amradelata <amradelata@gmail.com>
2020-02-02 21:51:40 +0200

    create react app/instull semple react snept/ bootstrap/create first componrnts

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component {
+  render() {
+    return (
+    <React.Fragment> 
+      <h1>Hi ther</h1>
+      <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter; 

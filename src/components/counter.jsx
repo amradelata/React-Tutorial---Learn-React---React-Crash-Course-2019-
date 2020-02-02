00788725031fcd5f3e94edfa22fef47078674c1f
amradelata <amradelata@gmail.com>
2020-02-02 22:36:19 +0200

    add style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,22 @@
 
   class Counter extends Component {
     state = {
-      count : 0
+      count : 0,
+      imgUrl : 'https://images.pexels.com/photos/3576955/pexels-photo-3576955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     };
+    styles = {color: 'red'};
     render() {
       return (
-        <div> 
-          <span>{this.formatCount()}</span>
-          <button>Increment</button>
+        <div>
+          <img src={this.state.imgUrl}/> 
+          <span style={this.styles} className="m-2 badge badge-primary">{this.formatCount()}</span>
+          <button style={{color: 'red'}} className="btn btn-secondary btn-sm">Increment</button>
         </div>
       );
     }
     formatCount() {
   const { count } = this.state;
-  return count === 0 ? <h1>Zero</h1> : count;
+  return count === 0 ? 'Zero' : count;
 }
   }
 

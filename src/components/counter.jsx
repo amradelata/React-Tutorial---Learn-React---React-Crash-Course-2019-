2c3db2d8b8bb32ba141527a433f67658bd9d4e0a
amradelata <amradelata@gmail.com>
2020-02-03 14:05:24 +0200

    to pass an argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,6 @@
     state = {
       count : 10,
       tags: [ {body: 'body1'},{body: 'body2'},{body: 'body3'}]
-      
     };
       renderTags() {
       // another way to to do if condition 
@@ -17,19 +16,22 @@
         super();
         this.handelIncrament = this.handelIncrament.bind(this)
       }
-      handelIncrament = () =>{
+      handelIncrament = product =>{
+        console.log(product)
         // console.log(this.state.count+=1)
         this.setState({count: this.state.count+=1})
-        
         // this.state.count+=1 
       }
+      // doHandelaincrument = () =>{
+      //   this.handelIncrament ({ id: 1});
+      // }
     render() {
       let classes = "m-2 p-2 badge-";
       classes += this.state.count === 0 ? 'warning' :  'primary';
       return (
         <div>
           <span className={classes}>{this.state.count}</span>
-          <button onClick={this.handelIncrament} className="btn btn-secondary btn-sm">Increment</button>
+          <button onClick={ ()=> this.handelIncrament(product)} className="btn btn-secondary btn-sm">Increment</button>
          {/* if this true this.state.tags.length === 0  do this<p>ther are no tags</p> */}
           {/* how we do if condition */}
           {this.state.tags.length === 0 &&<p>ther are no tags</p>}

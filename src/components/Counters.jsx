c8ce0df24ffa08a3ed9f58c29236e721b0a191f3
amradelata <amradelata@gmail.com>
2020-02-03 16:42:11 +0200

    delet logic

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -11,15 +11,18 @@ class Counters extends Component {
       {id: 5 , value: 0},
     ]
    };
-    handelDelet = () => {
-      console.log('delet')
+    handelDelet = (counterId) => {
+      // console.log('delet', counterId)
+      const counters =this.state.counters.filter(c => c.id != counterId);
+      this.setState({counters: counters})
+      // if the key like the value we can do it like this  this.setState({counters})
     }
   render() {
     return ( <div>
 
      {this.state.counters.map(counter => 
 
-     <Counter key={counter.id} value={counter.value} onDelet={this.handelDelet}>
+     <Counter key={counter.id} onDelet={this.handelDelet} counter={counter}>
        
      <h4>Title{counter.id}</h4>
        </Counter>
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@
 
   class Counter extends Component {
     state = {
-      value : this.props.value,
+      value : this.props.counter.value,
       tags: [ {body: 'body1'},{body: 'body2'},{body: 'body3'}]
     };
       renderTags() {
@@ -21,7 +21,6 @@
 
 
     render() {
-      console.log(this.props.value)
       let classes = "m-2 p-2 badge-";
       classes += this.state.count === 0 ? 'warning' :  'primary';
       return (
@@ -29,7 +28,7 @@
       {this.props.children}
           <span className={classes}>{this.state.value}</span>
           <button onClick={this.handelIncrament} className="btn btn-secondary btn-sm">Increment</button>
-          <button onClick={this.props.onDelet} className="btn btn-danger btn-sm">delet</button>
+          <button onClick={() => this.props.onDelet(this.props.counter.id)} className="btn btn-danger btn-sm">delet</button>
          {/* if this true this.state.tags.length === 0  do this<p>ther are no tags</p> */}
           {/* how we do if condition */}
           {this.state.tags.length === 0 &&<p>ther are no tags</p>}

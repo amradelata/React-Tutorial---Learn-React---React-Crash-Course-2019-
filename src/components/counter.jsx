730a5d94e07b8a1d68e55a642ea73a278da45f60
amradelata <amradelata@gmail.com>
2020-02-03 13:19:13 +0200

    onClick event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,18 +7,22 @@
       tags: []
       // {body: 'body1'},{body: 'body2'},{body: 'body3'}
     };
-    renderTags() {
+      renderTags() {
       // another way to to do if condition 
       if(this.state.tags.length === 0) return <p>ther are no tags</p>
-      
-      return <ul> { this.state.tags.map(tag => <li key={tag.body}>{tag.body}</li>)}</ul>
-         
-        
-    }
-    render() {
 
+      return <ul> { this.state.tags.map(tag => <li key={tag.body}>{tag.body}</li>)}</ul>
+      };
+      handelIncrament(){
+        // this.state.count = this.state.count +1
+      }
+    render() {
+      let classes = "m-2 p-2 badge-";
+      classes += this.state.count === 0 ? 'warning' :  'primary';
       return (
         <div>
+          <span className={classes}>{this.state.count}</span>
+          <button onClick={this.handelIncrament} className="btn btn-secondary btn-sm">Increment</button>
          {/* if this true this.state.tags.length === 0  do this<p>ther are no tags</p> */}
           {/* how we do if condition */}
           {this.state.tags.length === 0 &&<p>ther are no tags</p>}

0fbb0d5e7511ea00c4d802b1e814137f2013b2a4
amradelata <amradelata@gmail.com>
2020-02-03 17:37:19 +0200

    reset/ index

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -17,17 +17,35 @@ class Counters extends Component {
       this.setState({counters: counters})
       // if the key like the value we can do it like this  this.setState({counters})
     }
+    handelreset = () =>{
+      const counters =  this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+      });
+      this.setState({ counters });
+    };
+    handelincrment = counter => {
+      // console.log(counter)
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value++;
+      this.setState({counters})
+    }
   render() {
     return ( <div>
+      <button onClick={this.handelreset} className="btn btn-primary btn-sm">reset</button>
 
      {this.state.counters.map(counter => 
 
-     <Counter key={counter.id} onDelet={this.handelDelet} counter={counter}>
+     <Counter key={counter.id} onDelet={this.handelDelet} counter={counter} onIncrment={this.handelincrment}>
        
      <h4>Title{counter.id}</h4>
        </Counter>
      
      )}
+
+     
     </div> );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,25 @@
   import React, { Component } from 'react';
 
-
   class Counter extends Component {
-    state = {
-      value : this.props.counter.value,
-      tags: [ {body: 'body1'},{body: 'body2'},{body: 'body3'}]
-    };
-      renderTags() {
-      // another way to to do if condition 
-      if(this.state.tags.length === 0) return <p>ther are no tags</p>
-
-      return <ul> { this.state.tags.map(tag => <li key={tag.body}>{tag.body}</li>)}</ul>
-      };
-
-
-        handelIncrament = () =>{
-
-        this.setState({value: this.state.value + 1}) 
-        }
-
-
     render() {
       let classes = "m-2 p-2 badge-";
-      classes += this.state.count === 0 ? 'warning' :  'primary';
+      classes += this.props.counter.value === 0 ? 'warning' :  'primary';
       return (
         <div>
       {this.props.children}
-          <span className={classes}>{this.state.value}</span>
-          <button onClick={this.handelIncrament} className="btn btn-secondary btn-sm">Increment</button>
+      <span className={classes}>{this.props.counter.value}</span>
+          <button onClick={() => this.props.onIncrment(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
           <button onClick={() => this.props.onDelet(this.props.counter.id)} className="btn btn-danger btn-sm">delet</button>
-         {/* if this true this.state.tags.length === 0  do this<p>ther are no tags</p> */}
-          {/* how we do if condition */}
-          {this.state.tags.length === 0 &&<p>ther are no tags</p>}
-          {this.renderTags()}
+ 
         </div>
       );
     }
+
+
+      formatCount() {
+      const { value } = this.props.counter;
+      return value === 0 ? 'Zero' : value;
+      }
 
   }
 

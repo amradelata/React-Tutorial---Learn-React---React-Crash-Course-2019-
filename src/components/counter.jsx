2d35bf9574beb0a047007f6099d5788fad05c2b7
amradelata <amradelata@gmail.com>
2020-02-04 12:52:53 +0200

    Destructuring Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,15 @@
 
   class Counter extends Component {
     render() {
+      const { onDelet, onIncrment} = this.props;
       let classes = "m-2 p-2 badge-";
       classes += this.props.counter.value === 0 ? 'warning' :  'primary';
       return (
         <div>
       {this.props.children}
           <span className={classes}>{this.props.counter.value}</span>
-          <button onClick={() => this.props.onIncrment(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
-          <button onClick={() => this.props.onDelet(this.props.counter.id)} className="btn btn-danger btn-sm">delet</button>
+          <button onClick={() => onIncrment(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+          <button onClick={() => onDelet(this.props.counter.id)} className="btn btn-danger btn-sm">delet</button>
  
         </div>
       );
